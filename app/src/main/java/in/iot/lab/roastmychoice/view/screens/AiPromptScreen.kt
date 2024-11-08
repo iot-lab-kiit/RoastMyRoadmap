3708da6b6bf71f242382a4af480d650e1d754b07
Anirban Basak <jeetbasak2002@gmail.com>
2024-11-09 01:12:14 +0530

    refactor - V1 UI code refactoring in ai prompt screen

--- app/src/main/java/in/iot/lab/roastmychoice/view/screens/AiPromptScreen.kt
+++ app/src/main/java/in/iot/lab/roastmychoice/view/screens/AiPromptScreen.kt
@@ -2,18 +2,12 @@ package `in`.iot.lab.roastmychoice.view.screens
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Surface
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,10 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import `in`.iot.lab.roastmychoice.data.model.GetAiModelResponse
 import `in`.iot.lab.roastmychoice.state.UiState
+import `in`.iot.lab.roastmychoice.view.components.AppScreen
+import `in`.iot.lab.roastmychoice.view.components.PrimaryButton
 import `in`.iot.lab.roastmychoice.view.theme.GradientBrush
 import `in`.iot.lab.roastmychoice.view.events.AppEvents
 import `in`.iot.lab.roastmychoice.view.navigation.ONBOARDING_SCREEN
@@ -32,77 +27,79 @@ import `in`.iot.lab.roastmychoice.view.navigation.ONBOARDING_SCREEN
 
 @Composable
 fun AiPromptScreen(
-    navController: NavController,
     roastDataState: UiState<GetAiModelResponse>,
+    navController: NavController,
     setEvent: (AppEvents) -> Unit
 ) {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Black)
-        ) {
-            Box(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.CenterHorizontally)
-                    .border(
-                        border = BorderStroke(
-                            width = Dp.Hairline,
-                            brush = GradientBrush
-                        )
-                    )
-//                    .clip()
-            ) {
 
-                when (roastDataState) {
-                    is UiState.Idle -> {
-                        setEvent(AppEvents.FetchRoast)
-                    }
-
-                    is UiState.Loading -> {
-                        Text(text = "Loading")
-                    }
-
-                    is UiState.Success -> {
-                        Text(text = roastDataState.data.data)
-                    }
-
-                    else -> {
-                        Text(text = "Network Error")
-                    }
-                }
+    AppScreen {
+        when (roastDataState) {
+            is UiState.Idle -> {
+                setEvent(AppEvents.FetchRoast)
             }
 
-            Button(
-                onClick = {
-                    setEvent(AppEvents.DeleteUser)
-                    navController.navigate(ONBOARDING_SCREEN)
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .background(
-                        brush = GradientBrush,
-                        shape = RoundedCornerShape(16.dp)
-                    ),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                shape = RoundedCornerShape(16.dp),
-                contentPadding = PaddingValues()
-            ) {
-                Text(
-                    modifier = Modifier
-                        .padding(vertical = 16.dp),
-                    text = "Try Another",
-                    color = Color.White,
-                    fontSize = 22.sp
+            is UiState.Loading -> {
+                Text(text = "Loading")
+            }
+
+            is UiState.Success -> {
+                AiPromptScreenIdle(
+                    roastData = roastDataState.data,
+                    navController = navController,
+                    setEvent = setEvent
                 )
             }
 
+            else -> {
+                Text(text = "Network Error")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun AiPromptScreenIdle(
+    roastData: GetAiModelResponse,
+    navController: NavController,
+    setEvent: (AppEvents) -> Unit
+) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(
+            space = 24.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+    ) {
+
+        // Ai Generated Response
+        OutlinedCard(
+            modifier = Modifier.padding(24.dp),
+            onClick = {},
+            border = BorderStroke(
+                width = Dp.Hairline,
+                brush = GradientBrush
+            )
+        ) {
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = roastData.data
+            )
+        }
+
+        // Try Again Button
+        PrimaryButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            text = "Try Again"
+        ) {
+            setEvent(AppEvents.DeleteUser)
+            navController.navigate(ONBOARDING_SCREEN)
         }
     }
 }
